3bb506e6447752447d4031275b865b85939ffeae
xenlonmars <xenlonmars@proton.me>
2023-04-02 21:42:51 +0000

    site progress for web3 integration

---
+++ website/pages/web3.tsx
@@ -0,0 +1,13 @@
+import MetaMaskCard from '../components/connectorcards/MetaMaskCard'
+import ProviderExample from '../components/ProviderExample'
+
+export default function Home() {
+  return (
+    <>
+      <ProviderExample />
+      <div style={{ display: 'flex', flexFlow: 'wrap', fontFamily: 'sans-serif' }}>
+        <MetaMaskCard />
+      </div>
+    </>
+  )
+}
