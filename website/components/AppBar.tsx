92a9259d29f6a60df007d7871169a860572a82fc
xenlonmars <xenlonmars@proton.me>
2023-04-02 04:31:25 +0000

    on website link logo to home

--- website/components/AppBar.tsx
+++ website/components/AppBar.tsx
@@ -3,16 +3,19 @@ import AppBar from '@mui/material/AppBar';
 import Avatar from '@mui/material/Avatar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import Link from '../src/Link';
 
 export default function MainAppBar() {
   return (
     <AppBar elevation={0} position="relative">
-      <Toolbar>
-        <Avatar sx={{ mr: 2 }} src="/logo.png"></Avatar>
-        <Typography variant="h6" color="inherit" noWrap>
-          Xenlon Mars
-        </Typography>
-      </Toolbar>
+      <Link underline="none" href="/">
+        <Toolbar>
+          <Avatar sx={{ mr: 2 }} src="/logo.png"></Avatar>
+          <Typography variant="h6" color="inherit" noWrap>
+            Xenlon Mars
+          </Typography>
+        </Toolbar>
+      </Link>
     </AppBar>
   );
 }
--- website/components/Copyright.tsx
+++ website/components/Copyright.tsx
@@ -5,10 +5,10 @@ import Link from '@mui/material/Link';
 export default function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary" align="center">
-      {'Copyright © '}
       <Link color="inherit" href="https://www.xenlonmars.com/">
         Xenlon Mars
-      </Link>{' '}
+      </Link>{'. '}
+      Fair crypto principles.{' '}
       {new Date().getFullYear()}
       {'.'}
     </Typography>
