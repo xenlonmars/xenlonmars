c690f5da83a97037e3d9f6206d7f98b2860e0f20
xenlonmars <xenlonmars@proton.me>
2023-04-02 23:05:19 +0000

    update site out

--- website/chains.ts
+++ website/chains.ts
@@ -1,19 +1,19 @@
-import type { AddEthereumChainParameter } from '@web3-react/types'
+import type { AddEthereumChainParameter } from '@web3-react/types';
 
 interface BasicChainInformation {
-  urls: string[]
-  name: string
+  urls: string[];
+  name: string;
 }
 
 interface ExtendedChainInformation extends BasicChainInformation {
-  nativeCurrency: AddEthereumChainParameter['nativeCurrency']
-  blockExplorerUrls: AddEthereumChainParameter['blockExplorerUrls']
+  nativeCurrency: AddEthereumChainParameter['nativeCurrency'];
+  blockExplorerUrls: AddEthereumChainParameter['blockExplorerUrls'];
 }
 
 function isExtendedChainInformation(
   chainInformation: BasicChainInformation | ExtendedChainInformation
 ): chainInformation is ExtendedChainInformation {
-  return !!(chainInformation as ExtendedChainInformation).nativeCurrency
+  return !!(chainInformation as ExtendedChainInformation).nativeCurrency;
 }
 
 export function getAddChainParameters(chainId: number): AddEthereumChainParameter | number {
@@ -25,39 +25,40 @@ export function getAddChainParameters(chainId: number): AddEthereumChainParamete
       nativeCurrency: chainInformation.nativeCurrency,
       rpcUrls: chainInformation.urls,
       blockExplorerUrls: chainInformation.blockExplorerUrls,
-    }
+    };
   } else {
-    return chainId
+    return chainId;
   }
 }
 
 const getInfuraUrlFor = (network: string) =>
-  process.env.infuraKey ? `https://${network}.infura.io/v3/${process.env.infuraKey}` : undefined
+  process.env.infuraKey ? `https://${network}.infura.io/v3/${process.env.infuraKey}` : undefined;
 const getAlchemyUrlFor = (network: string) =>
-  process.env.alchemyKey ? `https://${network}.alchemyapi.io/v2/${process.env.alchemyKey}` : undefined
+  process.env.alchemyKey ? `https://${network}.alchemyapi.io/v2/${process.env.alchemyKey}` : undefined;
 
 type ChainConfig = { [chainId: number]: BasicChainInformation | ExtendedChainInformation }
 
 export const MAINNET_CHAINS: ChainConfig = {
   1: {
+    // @ts-ignore
     urls: [getInfuraUrlFor('mainnet'), getAlchemyUrlFor('eth-mainnet'), 'https://cloudflare-eth.com'].filter(Boolean),
     name: 'Mainnet',
   },
-}
+};
 
 export const CHAINS: ChainConfig = {
   ...MAINNET_CHAINS,
-}
+};
 
 export const URLS: { [chainId: number]: string[] } = Object.keys(CHAINS).reduce<{ [chainId: number]: string[] }>(
   (accumulator, chainId) => {
-    const validURLs: string[] = CHAINS[Number(chainId)].urls
+    const validURLs: string[] = CHAINS[Number(chainId)].urls;
 
     if (validURLs.length) {
-      accumulator[Number(chainId)] = validURLs
+      accumulator[Number(chainId)] = validURLs;
     }
 
-    return accumulator
+    return accumulator;
   },
   {}
-)
+);
