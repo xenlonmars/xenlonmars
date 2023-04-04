1f36906e17b83af84aa34527a8e712fb7a9b1b2a
xenlonmars <xenlonmars@proton.me>
2023-04-04 15:04:08 +0000

    site mint page fix approval

--- website/pages/mint.tsx
+++ website/pages/mint.tsx
@@ -61,7 +61,7 @@ export default function Checkout() {
       (async function () {
         try {
           const allowed = await dxn.allowance(account, addresses.ETHEREUM_MAINNET.XENLONMARS);
-          if (allowed <= 0) {
+          if (Number(ethers.formatEther(allowed.toString())) <= amountToBurn) {
             const tx = await dxn.approve(addresses.ETHEREUM_MAINNET.XENLONMARS, ethers.MaxUint256);
             (setAlertMessage as any)("please wait");
             await (provider as any).waitForTransaction(tx.hash);
